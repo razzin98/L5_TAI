335bc2d31e524c4b1b0a260c063b6f66d07cb123
razzin <mastertogo@vp.pl>
2021-03-26 10:54:44 +0100

    Transpilacja kodu TypeScript

--- complex.js
+++ complex.js
@@ -1,31 +1,31 @@
-var Complex = /** @class */ (function () {
-    function Complex(re, imag) {
+"use strict";
+class Complex {
+    constructor(re, imag) {
         this.re = re;
         this.imag = imag;
     }
-    Complex.prototype.getRe = function () {
+    getRe() {
         return this.re;
-    };
-    Complex.prototype.getImag = function () {
+    }
+    getImag() {
         return this.imag;
-    };
-    Complex.prototype.add = function (obj) {
+    }
+    add(obj) {
         return new Complex(this.re + obj.re, this.imag + obj.imag);
-    };
-    Complex.prototype.sub = function (obj) {
+    }
+    sub(obj) {
         return new Complex(this.re - obj.re, this.imag - obj.imag);
-    };
-    Complex.prototype.mod = function () {
+    }
+    mod() {
         return Math.sqrt((this.re * this.re) + (this.imag * this.imag));
-    };
-    Complex.prototype.write = function () {
+    }
+    write() {
         return this;
-    };
-    return Complex;
-}());
-var complex1 = new Complex(18, 10);
-var complex2 = new Complex(9, 5);
+    }
+}
+let complex1 = new Complex(18, 10);
+let complex2 = new Complex(9, 5);
 console.log('Dodawanie: ', complex1.getRe(), ', ', complex1.getImag(), 'i + ', complex2.getRe(), ', ', complex2.getImag(), 'i = ', complex1.add(complex2).write());
-var sub = complex1.sub(complex2);
+let sub = complex1.sub(complex2);
 console.log('Odejmowanie: ', complex1.getRe(), ', ', complex1.getImag(), 'i - ', complex2.getRe(), ', ', complex2.getImag(), 'i = ', complex1.sub(complex2).write());
 console.log("Moduł :", complex1.sub(complex2).mod());
