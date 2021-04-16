35b5268fc91a9e4e98f220d3eb7b6b08ba8589f8
razzin <mastertogo@vp.pl>
2021-04-16 15:17:58 +0200

    lab 06

--- blog/src/app/components/blog/blog.component.ts
+++ blog/src/app/components/blog/blog.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { DataService } from 'C:/Users/Veni/Desktop/ToiK/L5_TAI/blog/src/app/services/data.service';
 
 @Component({
-  selector: 'app-blog',
+  selector: 'blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+  @Input() filterText: string = ''
+  public items$: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private service: DataService) {
   }
 
+  ngOnInit() {
+    this.getAll();
+  }
+
+  getAll(){
+    this.service.getAll().subscribe(response => {
+      this.items$ = response;
+    });
+  }
 }
+
